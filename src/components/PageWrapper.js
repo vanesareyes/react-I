d688fc30313caa1fbb0088acb43e214f78a78895
vanesareyes <vanereyes22@gmail.com>
2020-02-20 17:11:31 -0300

    first commit

---
+++ src/components/PageWrapper.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PageWrapper() {
+    return ( <
+        div id = "wrapper" >
+        <
+        /div>
+    )
+}
+
+export default PageWrapper;
---
+++ src/components/mainContent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function mainContent() {
+    return ( <
+        div id = "content" >
+        <
+        /div>
+    )
+}
+
+export default mainContent;
